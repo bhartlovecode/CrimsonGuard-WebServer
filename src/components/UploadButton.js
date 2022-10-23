a5174fdb548fd165c2d41d6154042e2cea336eaf
Bradley Hartlove <bradleyhartlovecodes@gmail.com>
2022-10-23 18:20:05 -0400

    Changed button to file uploader

--- src/components/UploadButton.js
+++ src/components/UploadButton.js
@@ -3,8 +3,11 @@ import React from 'react'
 const UploadButton = () => {
   return (
     <div className="w-full grid inset-x-0">
-        <button className='bg-slate-800 hover:bg-red-700 text-white 
-            m-4 py-2 px-4 mx-auto rounded-full hover:bg-red w-1/5'>Upload File</button>
+        <label className="bg-slate-800 hover:bg-red-700 text-white w-1/5 
+            m-4 py-2 px-4 rounded-full hover:bg-red text-center mx-auto">
+            <input type="file"/>
+            Upload File
+        </label>
     </div>
   )
 }
